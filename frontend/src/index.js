cc5c04d53407bf43c4994fd9dc7985ae5270928c
srdjanRakic <srgjan.rakic@gmail.com>
2019-04-03 14:32:46 +0200

    Implement Apollo middleware

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import { setContext } from 'apollo-link-context';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+localStorage.removeItem('token');
+
+const authorizationMiddleware = setContext((operation, previousContext) => {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return previousContext;
+  }
+
+  return {
+    ...previousContext,
+    headers: {
+      ...previousContext.headers,
+      Authorization: `Bearer ${token}`
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authorizationMiddleware.concat(new HttpLink({ uri: 'http://localhost:8000/graphql' })),
+  cache: new InMemoryCache()
+});
+
+const AppWithProvider = () => (
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
+);
+
+
+ReactDOM.render(<AppWithProvider />, document.getElementById("root"));
